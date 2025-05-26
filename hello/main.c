aa194d0876d7880bee42c25bdaddba91b1ebf931
João Sena <joaopedromsena@gmail.com>
2025-05-26 16:07:04 -0300

    versão inicial

---
+++ hello/main.c
@@ -0,0 +1,33 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 João Sena <joaopedromsena@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  João Sena <joaopedromsena@gmail.com>
+ */
+ 
+#include <stdio.h>
+
+int main(void)   
+{
+  
+  // const char time[] = "DAD";
+  // printf ("Duda");
+  
+  int h = wtii();
+  
+  // while (time_i[i] != '\0') {
+  //   time_c[i] = (char)time_i[i];
+  //   i++;
+  // }
+  
+  return 0;
+}
+
+
+
+
+
---
+++ hello/stdio.h
@@ -0,0 +1,18 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 João Sena <joaopedromsena@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  João Sena <joaopedromsena@gmail.com>
+ */
+
+#ifndef E8_H
+#define E8_H
+
+void __attribute__((fastcall, naked)) printf(const char *);
+
+int __attribute__((naked)) wtii();
+
+#endif	
