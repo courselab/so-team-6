b5cefdf22fe9707f56fde726074a5edbac8be51c
João Sena <joaopedromsena@gmail.com>
2025-05-30 09:13:49 -0300

    versão 5

--- hello/main.c
+++ hello/main.c
@@ -14,33 +14,34 @@
 #define STR_SIZE 6
 
 const char *time_s;
-char time[STR_SIZE];
+char buffer[STR_SIZE];
 
-void convert(int h) {
+char *to_str(int t) {
   
+  // Inicializa o vetor de char com o caracter '0' em todas as posições (exceto na última)
   for (int i = (STR_SIZE - 2); i >= 0; i--) {
-    time[i] = '0';
+    buffer[i] = '0';
   }
   
+  buffer[(STR_SIZE - 1)] = '\0'; // Atribui '\0' na última posição, determinando o fim do vetor
+  
+  
+  // Calcula o char equivalente ao algarismo em todas as posições
   for (int i = (STR_SIZE - 2); i >= 0; i--) {
-    time[i] = (char)((h%10) + 48);
-    h /= 10;
+    buffer[i] = (char)((t%16) + 48);
+    t /= 16;
   }
   
-  time[(STR_SIZE - 1)] = '\0';
-  
-  time_s = time;
-  
-  return;
+  return buffer;
 }
 
 int main(void)   
 {
-  unsigned int time_i = wtii();
+  unsigned int time_i = wtii(); // O horário retornado convertido para hexadecimal considera o fuso UTC+0 (no Brasil é UTC-3)
   
-  convert(time_i);
+  time_s = to_str(time_i);
   
-  printf(time_s); // O horário retornado convertido para hexadecimal considera o fuso UTC+0 (no Brasil é UTC-3)
+  printf(time_s);
   printf("\r\n");
   
   return 0;
