bcd5d559e15b15178db836bb3cc02501b3582ed0
João Sena <joaopedromsena@gmail.com>
2025-06-26 13:29:14 -0300

    First

---
+++ mydos/kernel.c
@@ -0,0 +1,188 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 João Sena <joaopedromsena@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  João Sena <joaopedromsena@gmail.com>
+ */
+
+/* This source file implements the kernel entry function 'kmain' called
+   by the bootloader, and the command-line interpreter. Other kernel functions
+   were implemented separately in another source file for legibility. */
+
+#include "kernel.h" /* Essential kernel functions.  */
+#include "bios1.h"  /* For kwrite() etc.            */
+#include "bios2.h"  /* For kread() etc.             */
+#include "kaux.h"   /* Auxiliary kernel functions.  */
+
+/* Kernel's entry function. */
+
+void kmain(void)
+{
+  int i, j;
+
+  register_syscall_handler(); /* Register syscall handler at int 0x21.*/
+
+  splash(); /* Uncessary spash screen.              */
+
+  shell(); /* Invoke the command-line interpreter. */
+
+  halt(); /* On exit, halt.                       */
+}
+
+/* Tiny Shell (command-line interpreter). */
+
+char buffer[BUFF_SIZE];
+int go_on = 1;
+
+void shell()
+{
+  int i;
+  clear();
+  kwrite("TinyDOS 1.0\n");
+
+  while (go_on)
+  {
+
+    /* Read the user input.
+       Commands are single-word ASCII tokens with no blanks. */
+    do
+    {
+      kwrite(PROMPT);
+      kread(buffer);
+    } while (!buffer[0]);
+
+    /* Check for matching built-in commands */
+
+    i = 0;
+    while (cmds[i].funct)
+    {
+      if (!strcmp(buffer, cmds[i].name))
+      {
+        cmds[i].funct();
+        break;
+      }
+      i++;
+    }
+
+    /* If the user input does not match any built-in command name, just
+       ignore and read the next command. If we were to execute external
+       programs, on the other hand, this is where we would search for a
+       corresponding file with a matching name in the storage device,
+       load it and transfer it the execution. Left as exercise. */
+
+    if (!cmds[i].funct)
+      kwrite("Command not found\n");
+  }
+}
+
+/* Array with built-in command names and respective function pointers.
+   Function prototypes are in kernel.h. */
+
+struct cmd_t cmds[] =
+    {
+        {"help", f_help}, /* Print a help message.       */
+        {"quit", f_quit}, /* Exit TyDOS.                 */
+        {"hello", f_hello}, /* Execute an example program. */
+        {"list", f_list}, /* List the files in disk. */
+        {0, 0}};
+
+/* Build-in shell command: help. */
+
+void f_help()
+{
+  kwrite("...me, Obi-Wan, you're my only hope!\n\n");
+  kwrite("   But we can try also some commands:\n");
+  kwrite("      hello   (to run a sample user program\n");
+  kwrite("      quit    (to exit TyDOS)\n");
+  kwrite("      list    (to list the files in disk)\n");
+}
+
+void f_quit()
+{
+  kwrite("Program halted. Bye.\r\n");
+  go_on = 0;
+}
+
+/* List files in the volume.
+ * Arguments: (none)
+ */
+
+void f_list()
+{ 
+  // Header signature
+  // char h1 = *((int *)(0x7c00 + 0));
+  // char h2 = *((int *)(0x7c00 + 1));
+  // char h3 = *((int *)(0x7c00 + 2));
+  // char h3 = *((int *)(0x7c00 + 3));
+  
+  unsigned short total_number_of_sectors = *((int *)(0x7c00 + 4));  /* Number of 512-byte disk blocks.         */
+  unsigned short number_of_boot_sectors = *((int *)(0x7c00 + 6));   /* Sectors reserved for boot code.         */
+  unsigned short number_of_file_entries = *((int *)(0x7c00 + 8));   /* Maximum number of files in the disk.    */
+  unsigned short max_file_size = *((int *)(0x7c00 + 10));           /* Maximum size of a file in blocks.       */
+  int unused_space = *((int *)(0x7c00 + 12));                       /* Remaining space less than max_file_size.*/
+  
+  unsigned short number_of_entry_sectors = ((32 * number_of_file_entries) + (max_file_size * number_of_file_entries))/512 + 1;
+  
+  __asm__(
+    "mov boot_drive, %dl \n"	              /* Select the boot drive (from rt0.o). */
+    "mov $0x2, %ah \n"		              /* BIOS disk service: op. read sector. */
+    "mov $0x5, %al \n"   /* Number of sectors to read.          */
+    "mov $0x0, %ch \n"		              /* Cylinder coordinate (starts at 0).  */
+    "addb $0xb, %cl \n"   /* Sector coordinate - adds the number of boot sectors already read (starts at 1).  */
+    "mov $0x0, %dh \n"		              /* Head coordinage     (starts at 0).  */
+    "mov $_ENTRIES_ADDR, %bx \n"              /* Where to load the kernel (rt0.o).   */
+  );
+  
+  unsigned char c;
+  
+  for (int p = 0; p < 20; p++)
+  {
+    c = (char)(*((int *)(0x7c00 + (11 * 512) + p)));
+    
+    for(int i = 1; i >= 0; i--){
+      buffer[(p * 3) + i] = (char)((c%16 < 10) ? (c%16 + '0') : (c%16 - 10 + 'A'));
+      c /= 16;
+    }
+    buffer[(p * 3) + 2] = ' ';
+  }
+  buffer[60] = '\0';
+  kwrite(buffer);
+  kwrite("\n");
+  
+  
+  //for (int f = 0; f < number_of_file_entries; f++)
+  //{
+   // for (int i = 0; i < 32; i++)
+   // {
+  //    char l = *((int *)(0x7c00 + (number_of_boot_sectors * 512) + (32 * f) + i)); /* Memomy position of a letter 'i' in the entry file 'f' */
+  //    buffer[i] = l;
+  //  }
+ //   buffer[32] = '\0';
+  //  kwrite(buffer);
+  //  kwrite("\n");
+  //}
+}
+
+/* Built-in shell command: example.
+
+   Execute an example user program which invokes a syscall.
+
+   The example program (built from the source 'prog.c') is statically linked
+   to the kernel by the linker script (tydos.ld). In order to extend the
+   example, and load and external C program, edit 'f_exec' and 'prog.c' choosing
+   a different name for the entry function, such that it does not conflict with
+   the 'main' function of the external program.  Even better: remove 'f_exec'
+   entirely, and suppress the 'example_program' section from the tydos.ld, and
+   edit the Makefile not to include 'prog.o' and 'libtydos.o' from 'tydos.bin'.
+
+  */
+
+extern int main();
+void f_hello()
+{
+  main(); /* Call the user program's 'main' function. */
+}
